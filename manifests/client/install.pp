3b3a4bfd16793932fd66bd1671d71ede72ecd55b
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2016-11-24 12:25:21 +0100

    OpenBSD now comes with Puppet 4, but it doesn't have a separate gem installed under /opt/puppetlabs/...

--- manifests/client/install.pp
+++ manifests/client/install.pp
@@ -14,10 +14,20 @@ class ldap::client::install inherits ldap::client {
 
       # Puppet 4 has its own self-contained ruby environment so install the
       # requisite packages there
-      exec { '/opt/puppetlabs/puppet/bin/gem install net-ldap':
-        unless => '/opt/puppetlabs/puppet/bin/gem list | grep net-ldap',
+      case $::osfamily {
+        'OpenBSD': {
+          package { 'net-ldap':
+            ensure   => $ldap::client::net_ldap_package_ensure,
+            name     => $ldap::client::net_ldap_package_name,
+            provider => $ldap::client::net_ldap_package_provider,
+          }
+        }
+        default: {
+          exec { '/opt/puppetlabs/puppet/bin/gem install net-ldap':
+            unless => '/opt/puppetlabs/puppet/bin/gem list | grep net-ldap',
+          }
+        }
       }
-
     } else {
 
       package { 'net-ldap':
